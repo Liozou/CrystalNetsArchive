5639dbdb497562c196c7ca2dfd1cc04fe8cfc9b7
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2022-10-24 11:54:08 +0200

    fix miniscript to produce .tar.xz

--- miniscript.jl
+++ miniscript.jl
@@ -1,8 +1,10 @@
 using Tar, SHA, Inflate
 
 cd("/home/liozou/.julia/dev/CrystalNetsArchive/archives");
+run(`tar -cJf archives.tar.xz epinet.arc rcsr.arc zeolites.arc`);
 run(`tar -czf archives.tar.gz epinet.arc rcsr.arc zeolites.arc`);
 mv("archives.tar.gz", "../archives.tar.gz"; force=true);
+mv("archives.tar.xz", "../archives.tar.xz"; force=true);
 println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip("/home/liozou/.julia/dev/CrystalNetsArchive/archives.tar.gz"))));
 println("sha256 (tar.xz): ", bytes2hex(open(sha256, "/home/liozou/.julia/dev/CrystalNetsArchive/archives.tar.xz")));
 println("sha256 (zip): ", bytes2hex(open(sha256, "/home/liozou/.julia/dev/CrystalNetsArchive/archives.zip")));
