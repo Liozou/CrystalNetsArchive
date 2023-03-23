0f185b8f57f64095459d4bc31552e29e7b311397
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-03-23 13:30:15 +0100

    Automate zip

--- miniscript.jl
+++ miniscript.jl
@@ -1,10 +1,18 @@
-using Tar, SHA, Inflate
+using Tar, SHA, Inflate, ZipFile
 
 cd("/home/liozou/.julia/dev/CrystalNetsArchive/archives");
 run(`tar -cJf archives.tar.xz epinet.arc rcsr.arc zeolites.arc`);
 run(`tar -czf archives.tar.gz epinet.arc rcsr.arc zeolites.arc`);
 mv("archives.tar.gz", "../archives.tar.gz"; force=true);
 mv("archives.tar.xz", "../archives.tar.xz"; force=true);
+w = ZipFile.Writer("../archives.zip")
+for file in readdir(".");
+   f = ZipFile.addfile(w, file, method=ZipFile.Deflate)
+   for l in eachline(file)
+       println(f, l)
+   end
+end
+close(w)
 println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip("/home/liozou/.julia/dev/CrystalNetsArchive/archives.tar.gz"))));
 println("sha256 (tar.xz): ", bytes2hex(open(sha256, "/home/liozou/.julia/dev/CrystalNetsArchive/archives.tar.xz")));
 println("sha256 (zip): ", bytes2hex(open(sha256, "/home/liozou/.julia/dev/CrystalNetsArchive/archives.zip")));
